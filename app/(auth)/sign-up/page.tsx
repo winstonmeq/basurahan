0e82845e4c2e88f7de24773d48b8d13b64eadc83
Winston D. Mequila <winstonmequila@gmail.com>
2025-01-20 07:54:33 +0800

    Update page.tsx

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -30,8 +30,9 @@ export default function SignUp() {
 
 	const [pending, setPending] = useState(false);
 	const { toast } = useToast();
-	 const [role, setRole] = useState("user"); // Default role is "user"
+	 //const [role, setRole] = useState("user"); // Default role is "user"
 
+	const role = "user"
 	const form = useForm<z.infer<typeof signUpSchema>>({
 		resolver: zodResolver(signUpSchema),
 		defaultValues: {
